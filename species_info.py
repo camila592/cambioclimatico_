9eb574f546b90038dc26c2c88d7d74aafcc8aa49
camila592 <camila.aquili@gmail.com>
2025-09-18 19:06:10 -0300

    Create species_info.py

---
+++ species_info.py
@@ -0,0 +1,38 @@
+species_info = {
+    "Mariposa Monarca": {
+        "descripcion": "Migratoria, clave en polinización.",
+        "ayuda": "Plantar algodoncillo, reducir pesticidas.",
+        "beneficio": "Mantiene cadenas tróficas y poliniza flores."
+    },
+    "Oso Polar": {
+        "descripcion": "Amenazado por el deshielo del Ártico.",
+        "ayuda": "Reducir huella de carbono, apoyar reservas.",
+        "beneficio": "Especie paraguas que regula ecosistemas árticos."
+    },
+    "Tortuga Verde": {
+        "descripcion": "Habita mares tropicales y subtropicales, amenazada por la pesca.",
+        "ayuda": "Evitar plásticos, apoyar programas de conservación marina.",
+        "beneficio": "Controla poblaciones de medusas y mantiene praderas marinas."
+    },
+    "Flamenco andino": {
+        "descripcion": "Ave de gran tamaño que vive en humedales altoandinos de Sudamérica.",
+        "ayuda": "Controla poblaciones de algas y pequeños invertebrados, indicador de salud de humedales.",
+        "beneficio": "Proteger lagunas altoandinas, usar menos agua en zonas secas, evitar minería contaminante en su hábitat."
+    },
+    "Pinguino emperador": {
+        "descripcion": "El pingüino más grande, vive en la Antártida y depende del hielo marino.",
+        "ayuda": "Parte clave de la cadena alimenticia marina, controla poblaciones de peces y krill.",
+        "beneficio": "Reducir emisiones globales, apoyar áreas protegidas en la Antártida, consumir pescado de fuentes sostenibles."
+    },
+    "Rinoceronte negro": {
+        "descripcion": "Mamífero herbívoro africano, crítico en sabanas y pastizales.",
+        "ayuda": "Ayuda a moldear el paisaje, dispersa semillas y mantiene equilibrio en la vegetación.",
+        "beneficio": "Apoyar programas contra la caza furtiva, proteger áreas de sabana, promover turismo sostenible."
+    },
+    "Rana dorada de Panama": {
+        "descripcion": "Anfibio endémico de Panamá, muy vulnerable al cambio climático y enfermedades",
+        "ayuda": "Controla poblaciones de insectos, indicador clave de la salud ambiental.",
+        "beneficio": "Proteger bosques tropicales, apoyar criaderos en cautiverio, reducir el uso de pesticidas que contaminan ríos."
+    }
+
+}
